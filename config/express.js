79b90d9b76e7fb7bd19564be5e760a72a0bf6dd8
Samuel Joseph Karetla <ksamueljoseph006@gmail.com>
2024-04-03 21:50:54 +0530

    Created basic express config file

---
+++ config/express.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
+// app.use('/api', router);
+
+export default app;
